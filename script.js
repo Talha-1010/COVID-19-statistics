f18e0ef025e2ad29d9d12b094c9495bc996c6bc4
Talha-1010 <talhanasr3e@gmail.com>
2021-08-13 23:12:36 +0500

    linked google spreedsheet with tabletop.js and displayed chart

--- script.js
+++ script.js
@@ -7,17 +7,82 @@
                        callback: showInfo,
                        simpleSheet: true } )
     }
+
     
+    var dataArray;
+
     function showInfo(data, tabletop) {
       alert('Successfully processed!')
-        // console.log(data);
-        data.forEach(data => {
-        console.log(data.Country_code , data.New_cases);
-           
-        });
+      //  console.log(data[0]);
+      
+       google.charts.load('current', {'packages':['corechart']});
+       google.charts.setOnLoadCallback(drawChart);
+      //  console.log(data[0].Date_reported);
+
+        dataArray = new Array(data.length);
+      
+       
+        // Loop to create 2D array using 1D array
+        for (var i = 0; i < dataArray.length; i++) {
+          dataArray[i] = new Array(2);
+        }
+          
+        var h = 0;
+          
+        // Loop to initialize 2D array elements.
+        for (var i = 0; i < dataArray.length; i++) {
+            for (var j = 0; j < 2; j++) {
+              
+              if(i==0 && j==0)
+              {
+                dataArray[i][j] =  "Date_reported";
+              }
+              else if(i==0 && j==1)
+              {
+                dataArray[i][j] =  "New_cases";
+              }
+              else if( i>0 && j==0)
+              {
+                dataArray[i][j] = data[i].Date_reported;
+              }
+              else if( i>0 && j==1)
+              {
+                dataArray[i][j] = Number(data[i].New_cases);
+              }
+              
+            }
+        }
+
         
 
     }
+    
+
+    
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable(dataArray);
+
+      var options = {
+        title: 'Pakistan Covid-19 Stats',
+        curveType: 'function',
+        legend: { position: 'right' }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+      chart.draw(data, options);
+
+      localStorage.setItem("key",chart.container.innerHTML);
+      console.log(chart);
+
+    }
+
+    //assigning local storage
+    var chart = document.getElementById("curve_chart");
+    chart.innerHTML =localStorage.getItem("key");
+
+
+    
     
     window.addEventListener('DOMContentLoaded', init)
 
